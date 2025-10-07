65e2209bd906f5ecf762e06ff80312ad578630f4
Mark <74011034+thecodehippo@users.noreply.github.com>
2025-10-07 10:08:14 +0100

    Fix global is not defined error in production build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  define: {
+    global: 'globalThis',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
